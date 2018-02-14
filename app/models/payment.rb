bae6e4bac9d764f1da5c18a2ae2bd3fa4494c999
Prabin Manandhar <manandharprabin@gmail.com>
2018-02-14 12:29:35 +0100

    Payment with Stripe Added with Eur currency

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,6 +12,6 @@ class Payment < ApplicationRecord
 
 	def process_payment
 		customer = Stripe::Customer.create email: email, card: token
-		Stripe::Charge.create customer: customer.id, amount: 1000, description: 'Premium', currency: 'euro'
+		Stripe::Charge.create customer: customer.id, amount: 1000, description: 'Premium', currency: 'eur'
 	end
 end
